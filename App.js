650648a4baca3afb150e5257e6edbfd043d91e7d
Andre Staltz <andre@staltz.com>
2018-06-10 13:44:43 +0300

    init

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+
+export default class App extends React.Component {
+  state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    );
+  }
+}
