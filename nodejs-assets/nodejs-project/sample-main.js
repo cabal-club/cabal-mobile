7a10ad7aed2fc60afcc4cabf9748912b602d50a8
Alessandro Arnodo <alessandro@arnodo.net>
2018-06-27 10:07:23 +0200

    exclude sample-main.js from linter

--- nodejs-assets/nodejs-project/sample-main.js
+++ nodejs-assets/nodejs-project/sample-main.js
@@ -1,12 +1,12 @@
 // Rename this sample file to main.js to use on your project.
 // The main.js file will be overwritten in updates/reinstalls.
 
-var rnBridge = require('rn-bridge')
+var rn_bridge = require('rn-bridge');
 
 // Echo every message received from react-native.
-rnBridge.channel.on('message', (msg) => {
-  rnBridge.channel.send(msg)
-})
+rn_bridge.channel.on('message', (msg) => {
+  rn_bridge.channel.send(msg);
+} );
 
 // Inform react-native node is initialized.
-rnBridge.channel.send('Node was initialized.')
+rn_bridge.channel.send("Node was initialized.");
