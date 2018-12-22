9e6a2fea5c0b47558cefa119ed0e9bc375ff1e7c
Nick Warner <nickwarner@gmail.com>
2018-12-22 00:27:09 -0500

    Fix rebase

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import {AppRegistry} from 'react-native'
-=======
 import { AppRegistry } from 'react-native'
->>>>>>> WIP update everything and support kappacore based cabal-core
 import App from './frontend/App'
 
 AppRegistry.registerComponent('cabalmobile', () => App)
